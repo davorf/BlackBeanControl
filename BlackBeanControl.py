12bd420f322cc15c0d4ed85df8c83ec132e16dc8
davorf <davorf@gmail.com>
2016-10-31 14:08:57 +0100

    Configuration and error handling
    - Command line arguments for IP address and Port were missing argument
    requirement
    - Checking BlackBeanControl.ini and command line arguments for IP
    address and Port parameters
    - Error handling on ByteArray conversion and Socket connection

--- BlackBeanControl.py
+++ BlackBeanControl.py
@@ -11,7 +11,7 @@ AlternativeIPAddress = ''
 AlternativePort = ''
 
 try:
-    Options, args = getopt.getopt(sys.argv[1:], 'c:iph', ['command=','ipaddress=','port=','help'])
+    Options, args = getopt.getopt(sys.argv[1:], 'c:i:p:h', ['command=','ipaddress=','port=','help'])
 except getopt.GetoptError:
     print('BlackBeanControl.py -c <Command name> [-i <IP Address>] [-p <Port>]')
     sys.exit(2)
@@ -27,25 +27,54 @@ for Option, Argument in Options:
     elif Option in ('-p', '--port'):
         AlternativePort = Argument
 
+if SentCommand.strip() == '':
+    print('Command name parameter is mandatory')
+    sys.exit(2)
+
 if AlternativeIPAddress.strip() != '':
     RealIPAddress = AlternativeIPAddress.strip()
 else:
     RealIPAddress = Settings.IPAddress
+
+if RealIPAddress.strip() == '':
+    print('IP address must exist in BlackBeanControl.ini or it should be entered as a command line parameter')
+    sys.exit(2)
 
 if AlternativePort.strip() != '':
     RealPort = AlternativePort.strip()
 else:
     RealPort = Settings.Port
 
-CommandFromSettings = SettingsFile.get('Commands', SentCommand)
-FirstRandomPart = binascii.b2a_hex(os.urandom(2))
-SecondRandomPart = binascii.b2a_hex(os.urandom(2))
-CommandToSend = CommandFromSettings[0:64] + FirstRandomPart.decode('utf-8') + CommandFromSettings[68:80] + SecondRandomPart.decode('utf-8') + CommandFromSettings[84:432]
-        
-RMSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+if RealPort.strip() == '':
+    print('Port must exist in BlackBeanControl.ini or it should be entered as a command line parameter')
+    sys.exit(2)    
 
-UDPData = bytearray.fromhex(CommandToSend)
-RMSocket.connect((RealIPAddress, int(RealPort)))
-RMSocket.send(UDPData)
- 
-RMSocket.close()
+if SettingsFile.has_option('Commands', SentCommand):
+    CommandFromSettings = SettingsFile.get('Commands', SentCommand)
+else:
+    CommandFromSettings = ''
+
+if CommandFromSettings.strip() != '':
+    try:
+        FirstRandomPart = binascii.b2a_hex(os.urandom(2))
+        SecondRandomPart = binascii.b2a_hex(os.urandom(2))
+        CommandToSend = CommandFromSettings[0:64] + FirstRandomPart.decode('utf-8') + CommandFromSettings[68:80] + SecondRandomPart.decode('utf-8') + CommandFromSettings[84:432]
+                
+        RMSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+
+        try:
+            UDPData = bytearray.fromhex(CommandToSend)
+        except:
+            print('An error occured while trying to convert UDP payload to byte array')
+            sys.exit(1)
+            
+        RMSocket.connect((RealIPAddress, int(RealPort)))
+        RMSocket.send(UDPData)
+         
+        RMSocket.close()
+    except socket.error as SocketErrorMessage:
+        print('An error occured while trying to send UDP payload. Original message: %s' % SocketErrorMessage)
+        sys.exit(1)
+else:
+    print('Command not found in BlackBeanControl.ini')
+    
