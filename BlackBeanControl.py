5c9dcdf56461c34653f25c9662fb3f969b3fc5ba
davorf <davorf@gmail.com>
2016-11-18 23:24:16 +0100

    Additional BlackBeanControl.ini sections and parameters
    - Added possibility to have additional sections in BlackBeanControl.ini
    with alternative IP Address, Port, MAC Address and Timeout
    - Added Device Name and Timeout command line parameters
    - Device Name parameter must be used in conjunction with additional
    BlackBeanControl.ini section name
    - Added validation of parameters
    - Cleaned unused imports

--- BlackBeanControl.py
+++ BlackBeanControl.py
@@ -2,10 +2,9 @@
 
 import broadlink, configparser
 import sys, getopt
-import os, time
-import binascii
-import Settings
+import time, binascii
 import netaddr
+import Settings
 from os import path
 from Crypto.Cipher import AES
 
@@ -14,34 +13,95 @@ SettingsFile.optionxform = str
 SettingsFile.read(Settings.BlackBeanControlSettings)
 
 SentCommand = ''
+DeviceName=''
+DeviceIPAddress = ''
+DevicePort = ''
+DeviceMACAddres = ''
+DeviceTimeout = ''
 AlternativeIPAddress = ''
 AlternativePort = ''
 AlternativeMACAddress = ''
+AlternativeTimeout = ''
 
 try:
-    Options, args = getopt.getopt(sys.argv[1:], 'c:i:p:m:h', ['command=','ipaddress=','port=', 'macaddress=','help'])
+    Options, args = getopt.getopt(sys.argv[1:], 'c:d:i:p:m:t:h', ['command=','device=','ipaddress=','port=','macaddress=','timeout=','help'])
 except getopt.GetoptError:
-    print('BlackBeanControl.py -c <Command name> [-i <IP Address>] [-p <Port>] [-m <MAC Address>]')
+    print('BlackBeanControl.py -c <Command name> [-d <Device name>] [-i <IP Address>] [-p <Port>] [-m <MAC Address>] [-t <Timeout>]')
     sys.exit(2)
 
 for Option, Argument in Options:
     if Option in ('-h', '--help'):
-        print('BlackBeanControl.py -c <Command name> [-i <IP Address>] [-p <Port>] [-m <MAC Address>]')
+        print('BlackBeanControl.py -c <Command name> [-d <Device name>] [-i <IP Address>] [-p <Port>] [-m <MAC Address>] [-t <Timeout>')
         sys.exit()
     elif Option in ('-c', '--command'):
         SentCommand = Argument
+    elif Option in ('-d', '--device'):
+        DeviceName = Argument
     elif Option in ('-i', '--ipaddress'):
         AlternativeIPAddress = Argument
     elif Option in ('-p', '--port'):
         AlternativePort = Argument
     elif Option in ('-m', '--macaddress'):
         AlternativeMACAddress = Argument
+    elif Option in ('-t', '--timeout'):
+        AlternativeTimeout = Argument
 
 if SentCommand.strip() == '':
     print('Command name parameter is mandatory')
     sys.exit(2)
 
-if AlternativeIPAddress.strip() != '':
+if (DeviceName.strip() != '') and ((AlternativeIPAddress.strip() != '') or (AlternativePort.strip() != '') or (AlternativeMACAddress.strip() != '') or (AlternativeTimeout != '')):
+    print('Device name parameter can not be used in conjunction with IP Address/Port/MAC Address/Timeout parameters')
+    sys.exit(2)
+
+if (((AlternativeIPAddress.strip() != '') or (AlternativePort.strip() != '') or (AlternativeMACAddress.strip() != '') or (AlternativeTimeout.strip() != '')) and ((AlternativeIPAddress.strip() == '') or (AlternativePort.strip() == '') or (AlternativeMACAddress.strip() == '') or (AlternativeTimeout.strip() == ''))):
+    print('IP Address, Port, MAC Address and Timeout parameters can not be used separately')
+    sys.exit(2)
+
+if DeviceName.strip() != '':
+    if SettingsFile.has_section(DeviceName.strip()):
+        if SettingsFile.has_option(DeviceName.strip(), 'IPAddress'):
+            DeviceIPAddress = SettingsFile.get(DeviceName.strip(), 'IPAddress')
+        else:
+            DeviceIPAddress = ''
+
+        if SettingsFile.has_option(DeviceName.strip(), 'Port'):
+            DevicePort = SettingsFile.get(DeviceName.strip(), 'Port')
+        else:
+            DevicePort = ''
+
+        if SettingsFile.has_option(DeviceName.strip(), 'MACAddress'):
+            DeviceMACAddress = SettingsFile.get(DeviceName.strip(), 'MACAddress')
+        else:
+            DeviceMACAddress = ''
+
+        if SettingsFile.has_option(DeviceName.strip(), 'Timeout'):
+            DeviceTimeout = SettingsFile.get(DeviceName.strip(), 'Timeout')
+        else:
+            DeviceTimeout = ''        
+    else:
+        print('Device does not exist in BlackBeanControl.ini')
+        sys.exit(2)
+
+if (DeviceName.strip() != '') and (DeviceIPAddress.strip() == ''):
+    print('IP address must exist in BlackBeanControl.ini for the selected device')
+    sys.exit(2)
+
+if (DeviceName.strip() != '') and (DevicePort.strip() == ''):
+    print('Port must exist in BlackBeanControl.ini for the selected device')
+    sys.exit(2)
+
+if (DeviceName.strip() != '') and (DeviceMACAddress.strip() == ''):
+    print('MAC address must exist in BlackBeanControl.ini for the selected device')
+    sys.exit(2)
+
+if (DeviceName.strip() != '') and (DeviceTimeout.strip() == ''):
+    print('Timeout must exist in BlackBeanControl.ini for the selected device')
+    sys.exit(2)    
+
+if DeviceName.strip() != '':
+    RealIPAddress = DeviceIPAddress.strip()
+elif AlternativeIPAddress.strip() != '':
     RealIPAddress = AlternativeIPAddress.strip()
 else:
     RealIPAddress = Settings.IPAddress
@@ -50,7 +110,9 @@ if RealIPAddress.strip() == '':
     print('IP address must exist in BlackBeanControl.ini or it should be entered as a command line parameter')
     sys.exit(2)
 
-if AlternativePort.strip() != '':
+if DeviceName.strip() != '':
+    RealPort = DevicePort.strip()
+elif AlternativePort.strip() != '':
     RealPort = AlternativePort.strip()
 else:
     RealPort = Settings.Port
@@ -61,7 +123,9 @@ if RealPort.strip() == '':
 else:
     RealPort = int(RealPort.strip())
 
-if AlternativeMACAddress.strip() != '':
+if DeviceName.strip() != '':
+    RealMACAddress = DeviceMACAddress.strip()
+elif AlternativeMACAddress.strip() != '':
     RealMACAddress = AlternativeMACAddress.strip()
 else:
     RealMACAddress = Settings.MACAddress
@@ -72,18 +136,24 @@ if RealMACAddress.strip() == '':
 else:
     RealMACAddress = netaddr.EUI(RealMACAddress)
 
+if DeviceName.strip() != '':
+    RealTimeout = DeviceTimeout.strip()
+elif AlternativeTimeout.strip() != '':
+    RealTimeout = AlternativeTimeout.strip()
+else:
+    RealTimeout = Settings.Timeout
+
+if RealTimeout.strip() == '':
+    print('Timeout must exist in BlackBeanControl.ini or it should be entered as a command line parameter')
+    sys.exit(2)
+else:
+    RealTimeout = int(RealTimeout.strip())    
+
 RM3Device = broadlink.device((RealIPAddress, RealPort), RealMACAddress)
 RM3Device.auth()
 
 RM3Key = RM3Device.key
 RM3IV = RM3Device.iv
-
-TimeoutFromSettings = Settings.Timeout
-
-if TimeoutFromSettings.strip() == '':
-    RealTimout = 30
-else:
-    RealTimeout = int(TimeoutFromSettings.strip())
 
 if SettingsFile.has_option('Commands', SentCommand):
     CommandFromSettings = SettingsFile.get('Commands', SentCommand)
