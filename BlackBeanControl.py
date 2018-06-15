233ccd1f3997429433d6c37e7c81d928e98b1f36
adworse <wlaer@wlaer.com>
2018-06-15 19:31:34 +0300

    Timeout argument error fix

--- BlackBeanControl.py
+++ BlackBeanControl.py
@@ -164,7 +164,7 @@ if RealTimeout.strip() == '':
 else:
     RealTimeout = int(RealTimeout.strip())    
 
-RM3Device = broadlink.rm((RealIPAddress, RealPort), RealMACAddress)
+RM3Device = broadlink.rm((RealIPAddress, RealPort), RealMACAddress, RealTimeout)
 RM3Device.auth()
 
 if ReKeyCommand:
